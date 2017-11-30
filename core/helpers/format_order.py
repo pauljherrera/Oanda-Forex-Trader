488b1c62c0f7f3e4694c289ae787bfc9e2c93db3
Jesús Valdez <alonso6230@gmail.com>
2017-11-30 14:36:44 -0400

    Round values

---
+++ core/helpers/format_order.py
@@ -0,0 +1,13 @@
+"""
+Created on Thu Nov 30 17:52:00 2017
+
+@author: Avanti Financial Services.
+"""
+import numpy as np
+
+def round_values(trade, decimals=5):
+    trade['Entry Price'] = float(np.around([trade['Entry Price']], decimals=decimals))
+    trade['Stop Loss'] = float(np.around([trade['Stop Loss']], decimals=decimals))
+    trade['Target Price 1'] = float(np.around([trade['Target Price 1']], decimals=decimals))
+
+    return trade
--- core/trader.py
+++ core/trader.py
@@ -4,6 +4,8 @@ Created on Tue Nov 21 17:32:50 2017
 
 @author: Avanti Financial Services.
 """
+import numpy as np
+from core.helpers.format_order import round_values
 import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.accounts as accounts
 from oandapyV20.contrib.requests import (
@@ -21,7 +23,7 @@ class Trader:
     """
     Makes trades operations using the Oanda API wrapper.
     """
-    def __init__(self, accountID, api_client, instrument=None, 
+    def __init__(self, accountID, api_client, instrument=None,
                  leverage=100, *args, **kwargs):
         self.accountID = accountID
         self.client = api_client
@@ -29,17 +31,18 @@ class Trader:
         self.instrument = instrument
         self.leverage = leverage
         self.get_margin_available()
-        
+        self.round_value = 5 if not 'JPY' in self.instrument else 3
+
         # Initializing daemon for getting account balance
         scheduler = BackgroundScheduler()
         scheduler.add_job(self.get_margin_available , 'interval', minutes=1)
         scheduler.start()
-        
+
     def get_margin_available(self):
         a = accounts.AccountDetails(self.accountID)
         r = self.client.request(a)
         self.margin_available = float(r['account']['marginAvailable'])
-        
+
     def new_order(self, trade):
         """
         This class will be called by the strategy to place new orders.
@@ -52,30 +55,31 @@ class Trader:
             'Target Price 2': float,
             'Type of Trade': <'LONG'/'SHORT'>}
         """
+        trade = round_values(trade,self.round_value)
         units = self.margin_available * trade['Pct of Portfolio'] * self.leverage
-        
-        self._place_order(_type='limit', 
-                        units=floor(units * trade['TP1 vs TP2 Split']), 
+
+        self._place_order(_type='limit',
+                        units=floor(units * trade['TP1 vs TP2 Split']),
                         side=trade['Type of Trade'],
-                        instrument=self.instrument, 
-                        price=trade['Entry Price'], 
+                        instrument=self.instrument,
+                        price=trade['Entry Price'],
                         stop_loss=trade['Stop Loss'],
                         take_profit=trade['Target Price 1'])
-        
-        self._place_order(_type='limit', 
-                        units=floor(units * (1 - trade['TP1 vs TP2 Split'])), 
+
+        self._place_order(_type='limit',
+                        units=floor(units * (1 - trade['TP1 vs TP2 Split'])),
                         side=trade['Type of Trade'],
-                        instrument=self.instrument, 
-                        price=trade['Entry Price'], 
+                        instrument=self.instrument,
+                        price=trade['Entry Price'],
                         stop_loss=trade['Stop Loss'],
                         take_profit=trade['Target Price 2'])
-        
+
         print('\nNew orders opened.')
         print('Entry price: {}'.format(trade['Entry Price']))
         print('Stop Loss: {}'.format(trade['Stop Loss']))
         print('Take profits: {} , {}'.format(trade['Target Price 1'],
                                              trade['Target Price 2']))
-    
+
     def _place_order(self, _type='market', units=1000, side='LONG',
                     instrument='EUR_USD', price=None, stop_loss=None,
                     take_profit=None):
@@ -111,29 +115,29 @@ class Trader:
                                           stopLossOnFill=stop_loss
                                           ).data
             order = orders.OrderCreate(accountID=self.accountID, data=limitOrder)
-                                      
+
         return self.client.request(order)
-      
-    
+
+
 if __name__ == "__main__":
     """
     For testing purposes.
     """
     import oandapyV20
-    
+
     instrument='USD_JPY'
-    accountID = '101-001-1407695-002' 
+    accountID = '101-001-1407695-002'
     access_token = 'f9263a6387fee52f94817d6cd8dca978-d097b210677ab84fb58b4655a33eb25c'
     client = oandapyV20.API(access_token=access_token, environment="practice")
     trader = Trader(accountID, client, instrument=instrument)
-    
+
     units = 10000
     _type = 'limit'
     price = 0.75
     side = 'buy'
     stop_loss = 0.74
     take_profit = 0.76
-    
+
     trade = {'Entry Price': 111.08000000000001,
             'Pct of Portfolio': 0.01,
             'Stop Loss': 110.86500000000001,
@@ -142,9 +146,5 @@ if __name__ == "__main__":
             'Target Price 2': 185.14699999999999,
             'Type of Trade': 'LONG',
             'zone_index': 1193}
-    
+
     trader.new_order(trade)
-    
-    
-    
-    
