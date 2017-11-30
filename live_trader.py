9f887d7d99b3fd24bf496c98a541c6a679289f92
Jesús Valdez <alonso6230@gmail.com>
2017-11-30 13:35:11 -0400

    Command line instrument added.

--- live_trader.py
+++ live_trader.py
@@ -4,6 +4,7 @@ Created on Tue Nov 21 19:39:44 2017
 
 @author: Avanti Financial Services.
 """
+import sys
 import oandapyV20
 
 from core.data_feeder import OandaDataFeeder
@@ -18,36 +19,35 @@ class CustomStrategy(Strategy):
         """
         print('New bar')
         print(ETF_df, ETF1_df)
-        
-        
 
-        
+
+
+
 if __name__ == "__main__":
     # Config variables
     environment = configuration['Environment']
     accountID = configuration['AccountID']
     access_token = configuration['Token']
-    instrument = configuration['Instrument']
+    instrument = sys.argv[1].replace("-","")
     headers = {'instrument': instrument,
-                'params': {'granularity':"M{}".format(configuration['ETF']), 
+                'params': {'granularity':"M{}".format(configuration['ETF']),
                            'count':1000},
                 'access_token': access_token,
                 'environment':environment,
                 'ETF1': configuration['ETF1'],
-                'ETF': configuration['ETF']} 
-               
+                'ETF': configuration['ETF']}
+
     # Instantiate classes.
-    client = oandapyV20.API(access_token=access_token, 
+    client = oandapyV20.API(access_token=access_token,
                             environment="practice")
-    trader = Trader(accountID=accountID, api_client=client, 
+    trader = Trader(accountID=accountID, api_client=client,
                     instrument=instrument)
-    data_feeder = OandaDataFeeder(accountID=accountID, 
-                            api_client=client)   
+    data_feeder = OandaDataFeeder(accountID=accountID,
+                            api_client=client)
     strategy = CustomStrategy(**headers)
     strategy.trader = trader
     data_feeder.pub.register('new_data', strategy)
-    
+
     # Start data feeder.
     data_feeder.get_live_data(instrument=instrument)
     print('Trader succesfully initialized.')
-    
