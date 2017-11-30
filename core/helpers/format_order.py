6d9da6a82cb86e8d05689f131f7fd49c3ad44eaf
Jesús Valdez <alonso6230@gmail.com>
2017-11-30 14:38:22 -0400

    Round values added

--- core/helpers/format_order.py
+++ core/helpers/format_order.py
@@ -9,5 +9,5 @@ def round_values(trade, decimals=5):
     trade['Entry Price'] = float(np.around([trade['Entry Price']], decimals=decimals))
     trade['Stop Loss'] = float(np.around([trade['Stop Loss']], decimals=decimals))
     trade['Target Price 1'] = float(np.around([trade['Target Price 1']], decimals=decimals))
-
+    trade['Target Price 2'] = float(np.around([trade['Target Price 2']], decimals=decimals))
     return trade
